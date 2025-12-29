be57e4763ec4eba9feb99eb91c08ee4f0cabd17a
Artem Bunichev <tembun@bk.ru>
2025-12-29 16:49:29 +0300

    grab: Search fixed strings by default
    The change is similar to 6040d4156d2281f7ae08de7154223b92e83400f7.

--- grab.sh
+++ grab.sh
@@ -5,5 +5,7 @@
 #
 # The options are the same as those of grep(1).
 #
+# By default, it searches for fixed strings only (-F).  Use -E for regexp.
+#
 
-grep --exclude-dir=".git" -RIl "$@"
+grep --exclude-dir=".git" -FRIl "$@"
