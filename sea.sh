6040d4156d2281f7ae08de7154223b92e83400f7
Artem Bunichev <tembun@bk.ru>
2025-12-29 16:38:49 +0300

    sea: Search for fixed strings only by default
    Since that is what that I imply 99% of the time.
    It is still possible to use regular expressions with -E.

--- sea.sh
+++ sea.sh
@@ -5,5 +5,8 @@
 #
 # It's a wrapper for grep(1), thus, it accepts the same options.
 #
+# By default, it makes grep(1) search for fixed strings only (-F option), so
+# if you want to make use of regular expression, you should specify -E option.
+#
 
-GREP_COLOR="1;7" grep --exclude-dir=".git" --color=always -RnI "$@"
+GREP_COLOR="1;7" grep --exclude-dir=".git" --color=always -FRnI "$@"
