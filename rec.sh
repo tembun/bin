597379a0cfbdbd74330117f2b21bc27bb6fc5449
Artem Bunichev <tembun@bk.ru>
2025-12-30 14:02:26 +0300

    rec: Significantly simplified
    An option -f (framerate) was added as part of this.

--- rec.sh
+++ rec.sh
@@ -1,62 +1,133 @@
 #!/bin/sh
 
 #
-# cast -- do screencast in FreeBSD.
-#
-# An argument does specify a path for output file without extension.
-# The output container is `.mp4'.
+# rec -- record a screencast in FreeBSD.
 #
 # Options:
-#     -m: Capture microphone sound.
-#     -s: Capture screen (desktop) sound.
-# Only one of these options may be specified.
+# 	-m -- capture microphone sound.
+# 	-s -- capture system (desktop) sound.
+# 	-f -- framerate
+# The output is in .mp4 extension (the extension should not be specified as
+# part of the output name).
 #
 
-need_aud_restore=0
-aud_restore() {
-	mixer "$orig_rec.recsrc=set"
+progname=$(basename "$0" .sh)
+DEFAULT_FRAMERATE="30"
+DEFAULT_OUT_EXT="mp4"
+SND_SRC_MIC="mic"
+SND_SRC_DESK="desk"
+
+err()
+{
+	echo "${progname}: $@" 1>&2
+	exit 1
 }
 
-if echo "$1" |grep -q "^\-"; then
-	if echo "$1" |grep -q "[ms]"; then
-		aud="-f oss -i /dev/dsp"
-		
-		orig_rec=$(mixer \
-		    |grep src \
-		    |tr -s ' ' \
-		    |sed 's/ \([a-z]*\) .*/\1/')
-		need_aud_restore=1
-		trap aud_restore 1 2 15
-	fi
-	
-	if echo "$1" |grep -q "m"; then
-		device="monitor"
-	else if echo "$1" |grep -q "s"; then
-		device="mic"
-	fi
-	fi
-	shift
-	
-	mixer "$device.recsrc=set"
-fi
+usage()
+{
+	echo "usage: ${progname} [-f framerate] [-m | -s] mp4_output" 1>&2
+	exit 2
+}
 
+check_freebsd()
+{
+	[ $(uname) = "FreeBSD" ]
+}
+
+# check_prog prog
+check_prog()
+{
+	which "$1" >/dev/null 2>&1
+}
+
+# check_num value
+check_num()
+{
+	echo "$1" |grep -Eq '^[0-9]+$'
+}
+
+restore_aud() {
+	mixer "${orig_dev}".recsrc=set >/dev/null
+}
+
+restore_aud_if_needed()
+{
+	[ $need_aud_restore -ne 0 ] && restore_aud
+}
+
+# get_aud_opt snd_src
+get_aud_opt()
+{
+	[ -n "$1" ] && echo "-f oss -i /dev/dsp"
+}
+
+# prepare_aud_dev snd_src
+prepare_aud_dev()
+{
+	local snd_src="$1"
+	local dev
+	setvar orig_dev $(mixer \
+	    |grep src \
+	    |tr -s ' ' \
+	    |cut -d ' ' -f 2)
+	setvar need_aud_restore 1
+	if [ "${snd_src}" = "${SND_SRC_MIC}" ]; then
+		dev="monitor"
+	elif [ "${snd_src}" ]; then
+		dev="mic"
+	fi
+	mixer "${dev}".recsrc=set >/dev/null || return 1
+}
+
+# do_rec framerate aud_opt out
+do_rec()
+{
+	local framerate="$1"
+	local aud_opt="$2"
+	local out="$3"
+	ffmpeg \
+	    -video_size 1366x768 \
+	    -framerate "${framerate}" \
+	    -f x11grab -i :0 \
+	    ${aud_opt} \
+	    -crf 23 \
+	    -vcodec libx264 \
+	    -preset fast \
+	    "${out}"
+}
+
+handle_opts()
+{
+	local o
+	
+	while getopts "f:ms" o; do
+	case $o in
+	f)
+		check_num "$OPTARG" || \
+		    err "Framerate should be a number: $OPTARG"
+		setvar framerate "$OPTARG"
+		;;
+	m)	setvar snd_src "$SND_SRC_MIC" ;;
+	s)	setvar snd_src "$SND_SRC_DESK" ;;
+	?)	usage ;;
+	esac
+	done
+}
+
+framerate="${DEFAULT_FRAMERATE}"
+snd_src=""
+need_aud_restore=0
+orig_dev=""
+trap restore_aud_if_needed HUP INT TERM EXIT
+
+check_freebsd || err "This script can be run only on FreeBSD"
+check_prog mixer || err "mixer(8) not found"
+check_prog ffmpeg || err "ffmpeg(1) not found"
+handle_opts $@
+shift $((OPTIND - 1))
 out="$1"
-
-if [ -z "$out" ]; then
-	echo "[cast]: Which output name?" 1>&2
-	exit 1
-fi
-
-ffmpeg \
-    -video_size 1366x768 \
-    -framerate 30 \
-    -f x11grab -i :0 \
-    $aud \
-    -crf 22 \
-    -vcodec libx264 \
-    -preset fast \
-    "$out.mp4"
-
-if [ $need_aud_restore -ne 0 ]; then
-	aud_restore
-fi
+[ -z "$out" ] && usage
+aud_opt=$(get_aud_opt "${snd_src}")
+prepare_aud_dev "${snd_src}"
+out_full="${out}.${DEFAULT_OUT_EXT}"
+do_rec "${framerate}" "${aud_opt}" "${out_full}" && echo "${out_full}"
