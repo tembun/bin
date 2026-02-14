28d7ada99d7e32ae39508d7173b682fb54c17792
Artem Bunichev <tembun@bk.ru>
2026-02-14 23:22:01 +0300

    src: Completely rewrite and change the idea
    Now instead of gathering the data from the Makefiles under /usr/src,
    rely on DWARF information in debug files under /usr/lib/debug/.

--- src.sh
+++ src.sh
@@ -1,197 +1,228 @@
 #!/bin/sh
 
-# src -- locate source code for executable in FreeBSD.
 #
-# It determines the source code depending on information
-# that can be obtained from Makefiles under /usr/src.
-# Operations with Makefiles require certain time, that's
-# why it does it once and stores the information in the index
-# file (under /usr/local/share/src by default), that is later
-# issued for actual fast searching.
+# src -- find source code for FreeBSD base system executable.
+#
+# The corresponding source code file is sent to $EDITOR.
+# If file is a binary executable, the information about the source file will be
+# extracted from the debug-file located under /usr/lib/debug.
+# If file is an ASCII text executable, the source file is the file itself.
+# Options:
+#	-n		Just display the source code filename.
+#	-s symbol	Look for a symbol (defaults to 'main' for binaries).
+#
 
+progname=$(basename "${0}" .sh)
+DBG_DIR="/usr/lib/debug"
+DBG_EXT="debug"
+DWARFDUMP="llvm-dwarfdump19"
 
-progname=$(basename "$0" .sh)
-TOP="/usr/src"
-INDEX_DIR="/usr/local/share/src"
-INDEX_NAME="base.index"
-INDEX="$INDEX_DIR/$INDEX_NAME"
+usage()
+{
+	echo "usage: ${progname} [-n] [-s symbol] file ..." 1>&2
+	exit 2
+}
 
-# The program supports two forms of invocation:
-#  `-u [-v]': creating an index file that we will make use of when
-#             actually locating the sources.
-#             `-v' activates verbose mode - added lines will be printed.
-#  `[-a] file ...': find sources for specified files.
-#                   With `-a' all the found paths are printed.
-usage() {
-	echo "usage: $progname -u [-v]
-       src [-a] file ..."
+warn()
+{
+	echo "${progname}: ${@}" 1>&2
+}
+
+err()
+{
+	warn "${@}"
 	exit 1
 }
 
-if [ "$#" = "0" ]; then
-	usage
-fi
+ensure_prog()
+{
+	_path=$(which "${1}" 2>/dev/null)
+	test -n "${_path}" && test -x "${_path}" || err "You need ${1} to run this"
+}
 
-# Check if we have a source tree.
-if [ ! -d "$TOP" ]; then
-	echo "[src]: source code directory $TOP doesn't exist" 1>&2
-	exit 1
-fi
+# locate_src_ascii file
+locate_src_ascii()
+{
+	__src="${1}"
+	__src_line="1"
+}
 
-# The program is invoked in "update" mode, which creates an index file.
-# The index file consists of lines:
-#   `<executable path and its hard links, if any> . <source paths>'.
-# Example:
-#   `/bin/ed /bin/ed /bin/red . /usr/src/bin/ed'.
-if [ "$1" = "-u" ]; then
-	# If in the verbose mode, the actual lines that go to the index
-	# file will be directed to stdout as well.
-	if [ "$2" = "-v" ]; then
-		verbose="1"
+accumulate_no_debug_warns()
+{
+	__no_debug_warns="${__no_debug_warns}
+$(warn "${1}" 2>&1)"
+}
+
+flush_no_debug_warns()
+{
+	_warns=$(echo "${__no_debug_warns}" |sed '/^$/d')
+	test -n "${_warns}" && warn "${_warns}"
+}
+
+# locate_dbg_bin file
+locate_dbg_bin()
+{
+	_file="${1}"
+	__src_dbg="${DBG_DIR}${_file}.${DBG_EXT}"
+	if [ ! -f "${__src_dbg}" ]; then
+		accumulate_no_debug_warns "Debug file for ${_file} not found: ${__src_dbg}"
+		return 1
 	fi
-	# Delete previously created index file if it exists.
-	rm -f "$INDEX" 2>/dev/null
-	# Create a path for index if it doesn't exist.
-	mkdir -p "$INDEX_DIR" 2>/dev/null
-	if [ $? -ne 0 ]; then
-		echo "[src]: can not create directory for index file $INDEX_DIR" 1>&2
-		exit 1
+	return 0
+}
+
+# get_dbg_info dbg_file symbol
+get_dbg_info()
+{
+	__dbg_info=$(${DWARFDUMP} -n "${2}" "${1}" 2>/dev/null)
+}
+
+# locate_src_file dbg_info
+locate_src_file()
+{
+	__src=$(echo "${1}" \
+	    |grep "DW_AT_decl_file" \
+	    |sed -e 's/.*("//' -e 's/")//')
+}
+
+# locate_src_by_call_site dbg_file symbol dbg_info
+locate_src_file_by_call_site()
+{
+	_dbg_file="${1}"
+	_sym="${2}"
+	_dbg_info="${3}"
+	if echo "${_dbg_info}" |grep -q "DW_AT_GNU_all_call_sites"; then
+		__dbg_info=$(${DWARFDUMP} -cn "${_sym}" "${_dbg_file}" 2>/dev/null \
+		    |awk 'BEGIN {p=0}; {if($2=="DW_TAG_GNU_call_site") {p=1}; if (p==1) {print $0}}')
+		test -z "${__dbg_info}" && return 1
+		_call_site_sym=$(echo "${__dbg_info}" \
+		    |grep "DW_AT_abstract_origin" \
+		    |sed -e 's/.* "//' -e 's/")//')
+		get_dbg_info "${_dbg_file}" "${_call_site_sym}"
+		test -z "${__dbg_info}" && return 1
+		locate_src_file "${__dbg_info}"
+		return 0
 	fi
-	# Since index file will be situated under /usr/local/share, only
-	# root user will be able to modify the file. We want to check if
-	# we have appropriate permissions for this before we start.
-	if ! touch "$INDEX" 2>/dev/null; then
-		echo "[src]: can not create an index file at $INDEX" 1>&2
-		exit 1
+	return 1
+}
+
+# locate_src_bin file symbol
+locate_src_bin()
+{
+	_file="${1}"
+	_sym="${2}"
+	locate_dbg_bin "${_file}"
+	test ${?} -ne 0 && return 1
+	get_dbg_info "${__src_dbg}" "${_sym}"
+	if [ -z "${__dbg_info}" ]; then
+		warn "No debug information for symbol ${_sym} in ${__src_dbg}"
+		return 1
 	fi
-	# Collect all the Makefiles we possibly will be interested in.
-	# Also avoiding lots of Makefiles that are for tests.
-	for mf in $(find ${TOP}/bin/ ${TOP}/sbin/ \
-	                 ${TOP}/usr.bin/ ${TOP}/usr.sbin/ \
-	                 ${TOP}/libexec/ \
-	                 ${TOP}/secure/usr.bin/ ${TOP}/secure/usr.sbin/ \
-	                 ${TOP}/cddl/ ${TOP}/kerberos5/ \
-	            -type f \
-	            -name Makefile \
-	            -not -path "*\/*tests\/*" \
-	            -maxdepth 4)
-	do
-		mfd=$(dirname $mf)
-		# Now we want to filter the Makefiles we don't need.
-		# We're only looking for those ones that are for producing
-		# binaries (PROG or PROG_CXX) or scripts (man(1), for example,
-		# is actually a shell script, but we want to be able to search
-		# for its source too).
-		if grep -Eq "(PROG(_CXX)|SCRIPTS)?\??( |	)*=" $mf ; then
-			# Here we're obtaining paths that this Makefile searches through.
-			# It's stored in `.PATH' variable.
-			sea_paths=$(make -C "$mfd" \
-		                	-V '${.PATH}' \
-		                	-f $mf \
-		           	2>/dev/null \
-		           	|sed "s/\.//")
-	    	# If no such paths, then just skip this Makefile.
-	    	if [ "$sea_paths" = " ." ]; then
-	    		continue
-	    	fi
-	    	seas=" ."
-	    	# Now we try to filter out all the search paths
-	    	# that do not actually contain the source code.
-	    	# If the path does not have source code files for
-	    	# C, C++ programs or shell scripts, or there are
-	    	# no executable files (that usually go without extensions),
-	    	# then it's most likely not a source code directory.
-	    	for sea in $sea_paths;do
-	    		prog_files=$(find "$sea" -type f \
-	    		                \( -perm -111 \
-	    	                	-or -name "*\.c" \
-	    	                	-or -name "*\.cpp" \
-	    	                	-or -name "*\.cc" \
-	    	                	-or -name "*\.sh" \
-	    	                	-or -name "*\.y" \) \
-	    	                    -maxdepth 1)
-	    		if [ -n "$prog_files" ]; then
-					seas="$seas $sea"
-				fi
-	    	done
-	    	# Obtain binary destination path and its hard links.
-			bin_and_links=$(make -C "$mfd" \
-		                     	-V '${BINDIR}/${PROGNAME} ${LINKS}' \
-		                     	-f $mf \
-		                	2>/dev/null
-			)
-			# We have included scripts in our search along with binaries,
-			# but a lot of Makefiles produce scripts that are used for
-			# building, I guess, and they are not system-wide.
-			# We can tell it's one of such scripts if its search
-			# paths look like that:
-			if [ "$bin_and_links" = "/ " ] \
-			   || [ "$bin_and_links" = "/bin/ " ] \
-			   || [ "$bin_and_links" = "/sbin/ " ] \
-			   || [ "$bin_and_links" = "/usr/bin/ " ] \
-			   || [ "$bin_and_links" = "/usr/sbin/ " ]; then
-				continue
-			fi
-			# Build up an index line entry.
-			index_line=$(echo "$bin_and_links$seas" |sed 's/  / /g')
-			if [ "$verbose" = "1" ];
-			then echo "$index_line" |tee -a "$INDEX"
-			else echo "$index_line" >>"$INDEX"
-			fi
+	locate_src_file "${__dbg_info}"
+	if [ -z "${__src}" ]; then
+		locate_src_file_by_call_site "${__src_dbg}" "${_sym}" "${__dbg_info}"
+		if [ ${?} -ne 0 ] || [ -z "${__src}" ]; then
+			warn "No source file for symbol ${_sym} in ${__src_dbg}"
+			return 1
 		fi
+	fi
+	if [ ! -f "${__src}" ]; then
+		warn "Source file for ${sym} was found, but it doesn't exist: ${__src}"
+	fi
+	__src_line=$(echo "${__dbg_info}" \
+	    |grep "DW_AT_decl_line" \
+	    |sed -e 's/.*(//' -e 's/)//')
+	if [ -z "${__src_line}" ]; then
+		warn "No source file line found for symbol ${_sym} in ${__src_dbg}"
+		return 1
+	fi
+}
+
+# try_locate_src file symbol?
+try_locate_src()
+{
+	_filepath="${1}"
+	_sym="${2}"
+	_filetype=$(file -b "${_filepath}")
+	case "${_filetype}" in
+	*ASCII*)
+		locate_src_ascii "${_filepath}"
+		return 0
+		;;
+	ELF*)
+		test -z "${_sym}" && _sym="main"
+		locate_src_bin "${_filepath}" "${_sym}"
+		return ${?}
+		;;
+	?)
+		warn "Unsupported file type for ${_filepath}: ${_filetype}"
+		return 1
+		;;
+	esac
+}
+
+# try_locate_src file symbol?
+locate_src()
+{
+	_file="${1}"
+	_sym="${2}"
+	_filepath=$(which "${_file}" 2>/dev/null)
+	if [ ${?} -ne 0 ]; then
+		warn "Can't locate path for ${_file}"
+		return 1
+	fi
+	_candidates=$(find $(dirname "${_filepath}") -samefile "${_filepath}")
+	for _candidate in ${_candidates}; do
+		try_locate_src "${_candidate}" "${_sym}"
+		test ${?} -eq 0 && return 0
 	done
-	exit 0
-fi
+	flush_no_debug_warns
+	return 1
+}
 
-# If we've reached this place, this means we're in the
-# second invocation form (searching sources).
+# print_file_name filename line_number
+print_filename()
+{
+	echo "${1}:${2}"
+}
 
-# First, check if we have an index file.
-if [ ! -f "$INDEX" ]; then
-	echo "[src]: index file $INDEX not found" 1>&2
-	exit 1
-fi
-
-# In "print all" mode all the paths, that were found
-# (usually more than one path is found, when the program is
-# built of several modules or it also can be that the program
-# includes a source code of some external program).
-if [ "$1" = "-a" ]; then
-	all_mode="1"
-	shift
-	if [ "$#" = "0" ]; then
-		usage
+# serve_results src_file src_line
+serve_results()
+{
+	_src_file="${1}"
+	_src_line="${2}"
+	if [ "${print_name_only}" = "1" ]; then
+		print_filename "${_src_file}" "${_src_line}"
+		return
 	fi
-fi
+	if [ -n "${EDITOR}" ]; then
+		${EDITOR} +"${_src_line}" "${_src_file}"
+	else
+		print_filename "${_src_file}" "${_src_line}"
+		return
+	fi
+}
 
-# Search paths for specified programs one-by-one.
-tgts="$@"
-for tgt in $tgts;do
-	tgt_path=$(which $tgt 2>/dev/null)
-	if [ "$?" != "0" ]; then
-		echo "[src]: $tgt is not found" 1>&2
-		continue
-	fi
-	# Resolve symlinks (like tar(1) is a symlink to /usr/bin/bsdtar).
-	# And also escape characters like "[" and "" for executables
-	# like [(1) (which is also test(1)) (otherwise, they will be
-	# treated as part of regex sytax by grep(1)).
-	tgt_path=$(realpath "$tgt_path" \
-	           |sed -e 's/\[/\\\[/g' \
-	                -e 's/\./\\\./g')
-	# Search the target path in the index file.
-	# Looking only into the first part of line, where binaries and links.
-	srcs=$(grep "$tgt_path .*\. " "$INDEX")
-	if [ -z "$srcs" ]; then
-		echo "[src]: no sources found for $tgt" 1>&2
-		continue
-	fi
-	srcs=$(echo "$srcs" |sed 's/.* \. //')
-	# Print only first path or all of them if appropriate flag is set.
-	for src in $srcs;do
-		echo "$src"
-		if [ "$all_mode" != "1" ]; then
-			break
-		fi
+handle_opts()
+{
+	while getopts "ns:" _o; do
+		case "${_o}" in
+		n)	print_name_only=1 ;;
+		s)	sym="${OPTARG}" ;;
+		?)	usage ;;
+		esac
 	done
+}
+
+handle_opts ${@}
+shift $((OPTIND - 1))
+test ${#} -lt 1 && usage
+if [ ${#} -gt 1 ]; then
+	print_name_only=1
+	test -n "${sym}" && err "-s is only supported for single file argument"
+fi
+ensure_prog "${DWARFDUMP}"
+for file in "${@}"; do
+	locate_src "${file}" "${sym}"
+	test ${?} -eq 0 && serve_results "${__src}" "${__src_line}"
 done
