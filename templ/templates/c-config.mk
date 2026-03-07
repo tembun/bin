6576fdde50fa0b5ccb5d351cd858172dd9dd24b6
Artem Bunichev <tembun@bk.ru>
2026-03-07 21:49:08 +0300

    templ/c-config.mk: C program with config.h

---
+++ templ/templates/c-config.mk
@@ -0,0 +1,38 @@
+CC=		cc
+CFLAGS_CHECK=	-Wall -Wpedantic
+CFLAGS=
+CFLAGS_RELEASE=	${CFLAGS_CHECK} -O2
+CFLAGS_DBG=	-DDEBUG
+CFLAGS_DEBUG=	-g ${CFLAGS_DBG}
+CFLAGS_DEV=	${CFLAGS_CHECK} ${CFLAGS_DBG}
+INCS=		-I/usr/local/include
+LIBS=		-L/usr/local/lib
+PREFIX=		/usr/local
+BINDIR=		bin
+INSTALL=	install
+INSTALL_STRIP=	-s
+SRC=		
+HEADERS=	config.h
+SRC_ALL=	${SRC} ${HEADERS}
+BIN=		
+
+all: dev
+
+config.h: config.def.h
+	cp config.def.h config.h
+
+dev: ${SRC_ALL}
+	${CC} ${CFLAGS} ${CFLAGS_DEV} -o ${BIN} ${INCS} ${LIBS} ${SRC}
+
+debug: ${SRC_ALL}
+	${CC} ${CFLAGS} ${CFLAGS_DEBUG} -o ${BIN} ${INCS} ${LIBS} ${SRC}
+
+release: ${SRC_ALL}
+	${CC} ${CFLAGS} ${CFLAGS_RELEASE} -o ${BIN} ${INCS} ${LIBS} ${SRC}
+
+install: release
+	mkdir -p ${PREFIX}/${BINDIR}
+	${INSTALL} ${INSTALL_STRIP} ${BIN} ${PREFIX}/${BINDIR}
+
+clean:
+	rm -f ${BIN}
