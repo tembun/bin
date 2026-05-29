ab0ad9058694d75d011eb8c2a649bbec95657b49
Artem Bunichev <temcbun@gmail.com>
2026-05-29 18:38:58 +0300

    nolf: Handle files without trailing newlinf (LF)

---
+++ nolf.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+#
+# nolf -- handle files without trailing newline (LF).
+#
+
+progname=$(basename -- "${0}" .sh)
+
+warn()
+{
+	echo "${progname}: ${@}" 1>&2
+}
+
+usage()
+{
+	echo "usage: ${progname} [-n] [path ...]" 1>&2
+	exit 2
+}
+
+add_lf()
+{
+	local file="${1}"
+	echo >>"${file}"
+}
+
+handle_opts()
+{
+	local o
+	while getopts "n" o; do
+		case "${o}" in
+		n)	n_flag=1 ;;
+		?)	usage ;;
+		esac
+	done
+}
+
+handle_opts ${@}
+shift $((OPTIND - 1))
+paths="${@:-"."}"
+files=$(grep -RIl "" "${paths}")
+for file in ${files}; do
+	test "$(tail -c 1 "${file}")" || continue
+	if [ "${n_flag}" != "1" ]; then
+		add_lf "${file}" || warn "Cannot add newline to file: ${file}"
+	fi
+	echo "${file}"
+done
