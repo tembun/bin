4adc1f40a3d12053126283b739d6533d5e810567
Artem Bunichev <temcbun@gmail.com>
2026-07-12 19:17:11 +0300

    dac: Add shift mode

--- bin/dac.sh
+++ bin/dac.sh
@@ -7,10 +7,14 @@
 . "$(dirname $(readlink -f "${0}"))/../libexec/subr.sh"
 
 MODE_DIFF="diff"
+MODE_SHIFT="shift"
 define_usage -m "${MODE_DIFF}" "[-S] [-f fmt] [-u output_units] [date_1] [-f fmt] date_2"
+define_usage -m "${MODE_SHIFT}" "[-i input_fmt] [-o output_fmt] [-s start_date] [-u shift_units] [+-]shift"
 
+SHIFT_UNITS_DEFAULT="days"
 OUT_UNITS_DEFAULT="days"
 INPUT_FMT_DEFAULT="%m-%d"
+OUT_FMT_DEFAULT="%Y-%m-%d"
 
 OPTS_DIFF="Sf:u:"
 handle_opts_diff()
@@ -62,6 +66,40 @@ handle_mode_diff()
 	local diff_sec=$((sec_max - sec_min))
 	local diff_res=$(units -t "${diff_sec} seconds" "${Out_units}")
 	echo "${diff_sign}${diff_res}"
+}
+
+OPTS_SHIFT="i:o:s:u:"
+handle_opts_shift()
+{
+	case "${1}" in
+	i)	Input_fmt="${OPTARG}" ;;
+	o)	Out_fmt="${OPTARG}" ;;
+	s)	Start_date="${OPTARG}" ;;
+	u)	Shift_units="${OPTARG}" ;;
+	?)	usage ;;
+	esac
+}
+handle_mode_shift()
+{
+	test "${#}" -eq 1 || usage
+	: ${Input_fmt:="${INPUT_FMT_DEFAULT}"}
+	: ${Out_fmt:="${OUT_FMT_DEFAULT}"}
+	: ${Shift_units:="${SHIFT_UNITS_DEFAULT}"}
+	: ${Start_date:=$(now "${Input_fmt}")}
+	local shift_input="${1}"
+	local shift_sign="+"
+	local first_shift_char=$(char 0 "${shift_input}")
+	test "${first_shift_char}" = "-" && shift_sign="-"
+	local shift_input_abs="${shift_input}"
+	if [ "${first_shift_char}" = "+" ] || [ "${first_shift_char}" = "-" ]; then
+		shift_input_abs=$(echo "${shift_input}" |cut -c 2-)
+	fi
+	test -n "${shift_input_abs}" || err "Wrong shift: ${shift_input}"
+	local shift_sec=$(units -t "${shift_input_abs} ${Shift_units}" "seconds")
+	test "${shift_sign}" = "-" && shift_sec=$((-shift_sec))
+	local input_sec=$(date2 "${Start_date}" "${Input_fmt}" "%s")
+	local dest_sec=$((input_sec + shift_sec))
+	date2 "${dest_sec}" "%s" "${Out_fmt}"
 }
 
 test "${#}" -ne 0 || usage
