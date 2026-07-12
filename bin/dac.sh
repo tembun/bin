aa011b6666a2efea29336a5d677d913447095816
Artem Bunichev <temcbun@gmail.com>
2026-07-12 14:40:29 +0300

    dac: Make use of subr.sh modes

--- bin/dac.sh
+++ bin/dac.sh
@@ -6,13 +6,14 @@
 
 . "$(dirname $(readlink -f "${0}"))/../libexec/subr.sh"
 
-define_usage "[-S] [-f fmt] [-t to] [date_1] [-f fmt] date_2"
+MODE_DIFF="diff"
+define_usage -m "${MODE_DIFF}" "[-S] [-f fmt] [-t to] [date_1] [-f fmt] date_2"
 
 TO_DEFAULT="days"
 FMT_DEFAULT="%m-%d"
 
-OPTS="Sf:t:"
-handle_opts()
+OPTS_DIFF="Sf:t:"
+handle_opts_diff()
 {
 	case "${1}" in
 	S)	No_diff_sign=1 ;;
@@ -35,40 +36,50 @@ date2sec()
 	date -jf "${fmt}" "${date}" "+%s"
 }
 
-eval "${HANDLE_OPTS_EVAL}"
-test "${#}" -gt 0 || usage
-: ${To:="${TO_DEFAULT}"}
-if [ "${#}" -eq 1 ]; then
-	fmt1="${FMT_DEFAULT}"
-	date1=$(get_date "${fmt1}")
-	fmt2="${Fmt:-"${FMT_DEFAULT}"}"
-	date2="${1}"
-else
-	date1="${1}"
-	fmt1="${Fmt:-"${FMT_DEFAULT}"}"
-	Fmt=""
-	shift
+handle_mode_diff()
+{
+	local fmt1="" fmt2="" date1="" date2=""
 	eval "${HANDLE_OPTS_EVAL}"
 	test "${#}" -gt 0 || usage
-	fmt2="${Fmt:-"${FMT_DEFAULT}"}"
-	date2="${1}"
-fi
-sec1=$(date2sec "${date1}" "${fmt1}")
-sec2=$(date2sec "${date2}" "${fmt2}")
-if [ "${sec1}" -gt "${sec2}" ]; then
-	sec_max="${sec1}"
-	sec_min="${sec2}"
-	diff_sign="-"
-elif [ "${sec2}" -gt "${sec1}" ]; then
-	sec_max="${sec2}"
-	sec_min="${sec1}"
-	diff_sign="+"
-else
-	sec_max="${sec2}"
-	sec_min="${sec1}"
-	diff_sign=""
-fi
-test "${No_diff_sign}" = "1" && diff_sign=""
-diff_sec=$((sec_max - sec_min))
-diff=$(units -t "${diff_sec} seconds" "${To}")
-echo "${diff_sign}${diff}"
+	: ${To:="${TO_DEFAULT}"}
+	if [ "${#}" -eq 1 ]; then
+		fmt1="${FMT_DEFAULT}"
+		date1=$(get_date "${fmt1}")
+		fmt2="${Fmt:-"${FMT_DEFAULT}"}"
+		date2="${1}"
+	else
+		date1="${1}"
+		fmt1="${Fmt:-"${FMT_DEFAULT}"}"
+		Fmt=""
+		shift
+		eval "${HANDLE_OPTS_EVAL}"
+		test "${#}" -gt 0 || usage
+			fmt2="${Fmt:-"${FMT_DEFAULT}"}"
+			date2="${1}"
+	fi
+	local sec1=$(date2sec "${date1}" "${fmt1}")
+	local sec2=$(date2sec "${date2}" "${fmt2}")
+	local sec_max="" sec_min="" diff_sign=""
+	if [ "${sec1}" -gt "${sec2}" ]; then
+		sec_max="${sec1}"
+		sec_min="${sec2}"
+		diff_sign="-"
+	elif [ "${sec2}" -gt "${sec1}" ]; then
+		sec_max="${sec2}"
+		sec_min="${sec1}"
+		diff_sign="+"
+	else
+		sec_max="${sec2}"
+		sec_min="${sec1}"
+		diff_sign=""
+	fi
+	test "${No_diff_sign}" = "1" && diff_sign=""
+	local diff_sec=$((sec_max - sec_min))
+	local diff_res=$(units -t "${diff_sec} seconds" "${To}")
+	echo "${diff_sign}${diff_res}"
+}
+
+test "${#}" -ne 0 || usage
+mode="${1}"
+shift
+handle_mode_abbrev "${mode}" "${@}"
